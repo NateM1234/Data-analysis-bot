e8850dcabc4fd3cf1d1dc00bfbc3dd2d4dae291f
Nate12345678 <nate.ma10@icloud.com>
2026-04-15 21:50:55 +1000

    notes

---
+++ data/fetcher.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+
+#msft = yf.Ticker("MSFT")
+#print(msft.financials)
+
+#btc = yf.Ticker("BTC-USD")
+#print(btc.info["regularMarketPrice"])
+
+#data = yf.download(["MSFT", "TSLA", "APPLE"],start="2020-01-01", end="2026-01-01")
+#data["Close"].plot(title = "Close Prices Big 3")
+#plt.show()
+
+#msft = yf.Ticker("MSFT")
+#hist = msft.history(period="1mo")
+#print(hist)
+#print(type(hist))
+#print(hist.columns)
+
+#data = yf.download("MSFT", period="1mo")
+#print(data)
+#print(type(data))
