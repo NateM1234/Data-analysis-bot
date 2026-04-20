66e5945b6cc38ed5a5e2078cc1b00b3b76d3f152
Nate12345678 <nate.ma10@icloud.com>
2026-04-20 21:39:43 +1000

    Updated indicators

--- analysis/indicators.py
+++ analysis/indicators.py
@@ -30,13 +30,46 @@ def add_indicators(data):
     return data
 
 def generate_signals(data):
-    pass
+    if data is None:
+        return None
+    
+    data['Signal'] = 'hold'    
+    for i in range(len(data)):
+        if (
+            data['RSI'].iloc[i] < 60 and 
+            data['RSI'].iloc[i] > 40 and 
+            data['SMA_20'].iloc[i] > data['SMA_50'].iloc[i] and
+            data['Volume'].iloc[i] > data['Volume_MA20'].iloc[i] and
+            data['Volatility'].iloc[i] < data['Volatility_MA30'].iloc[i]
 
+        ):
+            data.at[data.index[i], 'Signal'] = 'Buy'
 
-def apply_stop_loss(data):
-    pass
+        elif (
+            data['RSI'].iloc[i] > 75 and  
+            data['SMA_20'].iloc[i] < data['SMA_50'].iloc[i]
 
-def position_sizing(data, base_size=1.0):
-    data['Position_Size'] = base_size / data['Volatility']
+        ):
+            data.at[data.index[i], 'Signal'] = 'Sell'
+
     return data
+
+
+def apply_stop_loss(entry_price, current_price, threshold=0.07):
+    pct_drop = (entry_price - current_price) / entry_price
+
+    return pct_drop >= threshold
+   
+
+def position_sizing(capital, volatility, risk_pct=0.02):
+
+    
+    
+    if volatility == 0:
+        return 0
+    risk_amount = capital * risk_pct
+    
+    position_size = risk_amount / volatility
+    
+    return position_size
 
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime
 from data.fetcher import fetch_user_data, clean_data
-
+from analysis.indicators import add_indicators, generate_signals, apply_stop_loss, position_sizing
 
 ticker = input("Input ticker (e.g. MSFT): ")
 raw = fetch_user_data(ticker)
 clean = clean_data(raw)
+data = add_indicators(clean)
+data = generate_signals(data)
+
+print(data[['Close', 'RSI', 'SMA_20', 'SMA_50', 'Signal']].tail(10))
 
 if clean is not None:
     print(clean.head())
